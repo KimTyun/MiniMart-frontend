23e0159e85456e155d548cc0f9744bff58b07d12
min239 <gksckdalsmark@naver.com>
2025-08-21 11:51:51 +0900

    판매자 페이지에서 상품 클릭시 이동

--- src/pages/seller/SellerPageItems.jsx
+++ src/pages/seller/SellerPageItems.jsx
@@ -1,13 +1,21 @@
-import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import '../../styles/sellerPageItems.css'
 
 function SellerPageItems({ items }) {
+   const navigate = useNavigate()
    if (!items || items.length === 0) return <div>상품이 없습니다.</div>
-   
+   console.log(items)
+
    return (
       <div className="seller_items">
          {items.map((item) => (
-            <div key={item.id} className="seller_card">
+            <div
+               key={item.id}
+               className="seller_card"
+               onClick={() => {
+                  navigate(`/item/${item.id}`)
+               }}
+            >
                <img src={`${import.meta.env.VITE_API_URL}${item.ItemImgs[0].img_url}`} alt={item.name} className="seller_item_img" />
                <div>{item.name}</div>
                <div>{item.price}원</div>
