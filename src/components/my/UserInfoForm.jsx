8a4d2c9d4b272e5aec2eb781e960de3ebdddc599
min239 <gksckdalsmark@naver.com>
2025-08-21 11:59:49 +0900

    eslint 2개 빼고 해결

--- src/components/my/UserInfoForm.jsx
+++ src/components/my/UserInfoForm.jsx
@@ -115,9 +115,10 @@ const UserInfoForm = () => {
       try {
          const uploadedImageUrl = await uploadProfileImage(file)
          setFormData((prev) => ({ ...prev, profile_img: uploadedImageUrl }))
-      } catch (_) {
+      } catch (error) {
          setModalState({ show: true, message: '이미지 업로드 실패', isConfirm: false })
          setPreviewImage(originalData?.profile_img ? `${API_BASE_URL}${originalData.profile_img}` : `${API_BASE_URL}/uploads/profile-images/default.png`)
+         console.log(error)
       }
    }
 
